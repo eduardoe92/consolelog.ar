05a31dee29382ae1dc61b3c1cd117a7dabaa80c6
eduardoe92 <eduardo.elias.1192@gmail.com>
2023-11-01 18:48:00 -0300

    blur

--- blur.js
+++ blur.js
@@ -3,10 +3,7 @@ const boton = document.getElementById("boton");
 const tarjetas = document.getElementById("tarjetas");
 const cierraTarjetas = document.getElementById("cierraTarjetas");
 
-document.addEventListener("DOMContentLoaded", () => {
-    tarjetas.style.opacity = "1";
-    tarjetas.style.filter = "blur(0rem)";
-})
+
 
 document.addEventListener("keydown", () => {
     let escape = event.key;
@@ -33,24 +30,24 @@ cierraTarjetas.addEventListener("click", () => {
     elemento.style.transition = "1s";
 })
 
+
+
 boton.addEventListener("click", () => {
 
     if (elemento.classList.contains("blur")) {
         tarjetas.classList.remove("blur");
         tarjetas.style.transition = "1s";
-        tarjetas.style.opacity = "0";
-        tarjetas.style.visibility = "hidden";
-        tarjetas.style.filter = "blur(1.5rem)";
+        
         elemento.classList.remove("blur");
         elemento.style.filter = "blur(0rem)";
         elemento.style.transition = "1s";
     } else {
         tarjetas.style.overflow = "scroll";
         tarjetas.style.filter = "blur(0rem)";
-        tarjetas.style.transition = "1s";
         tarjetas.style.visibility = "visible";
         tarjetas.style.opacity = "1";
         tarjetas.style.transition = "1s";
+
         elemento.classList.add("blur");
         elemento.style.filter = "blur(1.5rem)";
         elemento.style.transition = "1s";
